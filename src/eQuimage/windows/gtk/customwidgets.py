ded5ce1081c355c4a35644e2ba731ead1c646aaa
ymniquet <ymniquet@gmail.com>
2024-03-01 10:06:03 +0100

    Added custom HBox and VBox containers.

--- src/eQuimage/windows/gtk/customwidgets.py
+++ src/eQuimage/windows/gtk/customwidgets.py
@@ -11,6 +11,52 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, GObject
 from .signals import Signals
 
+##########
+# Boxes. #
+##########
+
+class HBox(Gtk.HBox):
+  """Gtk horizontal box with default settings & wrappers."""
+
+  def __init__(self, *args, **kwargs):
+    """Initialize a Gtk.HBox with default spacing = 8."""
+    kwargs.setdefault("spacing", 8)
+    Gtk.HBox.__init__(self, *args, **kwargs)
+
+  def pack(self, widget, expand = False, fill = False, padding = 0):
+    """Wrapper for Gtk.HBox.pack_start(widget, expand, fill, padding) with default expand = False, fill = False and padding = 0."""
+    self.pack_start(widget, expand, fill, padding)
+
+class VBox(Gtk.VBox):
+  """Gtk vertical box with default settings & wrappers."""
+
+  def __init__(self, *args, **kwargs):
+    """Initialize a Gtk.VBox with default spacing = 16."""
+    kwargs.setdefault("spacing", 16)
+    Gtk.HBox.__init__(self, *args, **kwargs)
+
+  def pack(self, widget, expand = False, fill = False, padding = 0):
+    """Wrapper for Gtk.VBox.pack_start(widget, expand, fill, padding) with default expand = False, fill = False and padding = 0."""
+    self.pack_start(widget, expand, fill, padding)
+
+def make_hbox(widget, pre = None, post = None, spacing = 8, expand = False):
+  """Return a HBox with widgets 'pre', 'widget', and 'post' spaced by 'spacing'.
+     If strings, 'pre' and 'post' are converted into Gtk labels.
+     The 'widget' is packed as expandable & filling if 'expand' is True."""
+  hbox = HBox(spacing = spacing)
+  if pre is not None:
+    if isinstance(pre, str): pre = Gtk.Label(pre)
+    hbox.pack(pre)
+  hbox.pack(widget, expand = expand, fill = expand)
+  if post is not None:
+    if isinstance(post, str): post = Gtk.Label(post)
+    hbox.pack(post)
+  return hbox
+
+############
+# Buttons. #
+############
+
 class Button(Signals, Gtk.Button):
   """A custom Gtk button with extended signal management."""
 
@@ -30,24 +76,25 @@ class HoldButton(Signals, Gtk.Button):
   __gsignals__ = {"hold": (GObject.SIGNAL_RUN_LAST, GObject.TYPE_NONE, ())}
 
   def __init__(self, *args, **kwargs):
-    if "delay" in kwargs.keys():
-      self.delay = kwargs["delay"]
-      del kwargs["delay"]
-    else:
-      self.delay = 500
+    """Initialize class.
+       The button hold delay can be specified as kwarg 'delay' (default 500 ms)."""
+    self.delay = kwargs.pop("delay", 500)
     Signals.__init__(self)
     Gtk.Button.__init__(self, *args, **kwargs)
-    self.connect("pressed", self.pressed)
-    self.connect("released", self.released)
+    self.connect("pressed", self.__pressed)
+    self.connect("released", self.__released)
 
-  def pressed(self, widget):
-    self.timer = GObject.timeout_add(self.delay, self.longpressed)
+  def __pressed(self, widget):
+    """Internal callback for button press events."""
+    self.timer = GObject.timeout_add(self.delay, self.__longpressed)
 
-  def longpressed(self):
+  def __longpressed(self):
+    """Internal callback for button long press events."""
     self.emit("hold")
     return True
 
-  def released(self, widget):
+  def __released(self, widget):
+    """Internal callback for button release events."""
     GObject.source_remove(self.timer)
 
 #
@@ -89,7 +136,9 @@ class RadioButton(Signals, Gtk.RadioButton):
     self.set_active(*args, **kwargs)
     self.unblock_all_signals()
 
-#
+##########################
+# Spin buttons & scales. #
+##########################
 
 class SpinButton(Signals, Gtk.SpinButton):
   """A custom Gtk spin button with extended signal management."""
@@ -111,17 +160,9 @@ class SpinButton(Signals, Gtk.SpinButton):
     self.unblock_all_signals()
 
   def hbox(self, pre = None, post = None, spacing = 8):
-    """Return a Gtk HBox with Gtk widget 'pre', the spin button, and Gtk widget 'post', spaced by 'spacing'.
+    """Return a HBox with Gtk widget 'pre', the spin button, and Gtk widget 'post', spaced by 'spacing'.
        If strings, 'pre' and 'post' are converted into Gtk labels."""
-    hbox = Gtk.HBox(spacing = spacing)
-    if pre is not None:
-      if isinstance(pre, str): pre = Gtk.Label(pre)
-      hbox.pack_start(pre, False, False, 0)
-    hbox.pack_start(self, False, False, 0)
-    if post is not None:
-      if isinstance(post, str): post = Gtk.Label(post)
-      hbox.pack_start(post, False, False, 0)
-    return hbox
+    return make_hbox(self, pre, post, spacing, False)
 
 #
 
@@ -157,17 +198,9 @@ class HScale(Signals, Gtk.Scale):
     self.unblock_all_signals()
 
   def hbox(self, pre = None, post = None, spacing = 8):
-    """Return a Gtk HBox with Gtk widget 'pre', the horizontal scale, and Gtk widget 'post', spaced by 'spacing'.
+    """Return a HBox with Gtk widget 'pre', the horizontal scale, and Gtk widget 'post', spaced by 'spacing'.
        If strings, 'pre' and 'post' are converted into Gtk labels."""
-    hbox = Gtk.HBox(spacing = spacing)
-    if pre is not None:
-      if isinstance(pre, str): pre = Gtk.Label(pre)
-      hbox.pack_start(pre, False, False, 0)
-    hbox.pack_start(self, self.expand, self.expand, 0)
-    if post is not None:
-      if isinstance(post, str): post = Gtk.Label(post)
-      hbox.pack_start(post, False, False, 0)
-    return hbox
+    return make_hbox(self, pre, post, spacing, self.expand)
 
 #
 
@@ -182,11 +215,11 @@ class HScaleSpinButton():
     self.scale = HScale(value, minimum, maximum, step, page = page, digits = digits, length = length, expand = expand)
     self.button = SpinButton(value, minimum, maximum, step, page = page, digits = digits, climbrate = climbrate)
     self.scale.set_draw_value(False)
-    self.scale.connect("value-changed", lambda scale: self.__sync__(self.button, self.scale))
-    self.button.connect("value-changed", lambda button: self.__sync__(self.scale, self.button))
+    self.scale.connect("value-changed", lambda scale: self.__sync(self.button, self.scale))
+    self.button.connect("value-changed", lambda button: self.__sync(self.scale, self.button))
     self.__callbacks__ = {}
 
-  def __sync__(self, target, source):
+  def __sync(self, target, source):
     """Synchronize the value of widget 'target' with the value of widget 'source'."""
     target.set_value_block(source.get_value())
     callback = self.__callbacks__.get("value-changed", None)
@@ -213,26 +246,28 @@ class HScaleSpinButton():
 
   def layout1(self, label = None, spacing = 8):
     """Single line layout.
-       Return a Gtk box with label 'label' (if not None), the Gtk scale and the spin button, spaced by 'spacing'."""
-    hbox = Gtk.HBox(spacing = spacing)
-    if label is not None: hbox.pack_start(Gtk.Label(label = label), False, False, 0)
-    hbox.pack_start(self.scale, self.expand, self.expand, 0)
-    hbox.pack_start(self.button, False, False, 0)
+       Return a HBox with label 'label' (if not None), the Gtk scale and spin button, spaced by 'spacing'."""
+    hbox = HBox(spacing = spacing)
+    if label is not None: hbox.pack(Gtk.Label(label = label))
+    hbox.pack(self.scale, expand = self.expand, fill = self.expand)
+    hbox.pack(self.button)
     return hbox
 
   def layout2(self, label = "", spacing = 8):
     """Two lines layout.
-       Return a Gtk box with label 'label' and the Gtk spin button on one line (spaced by 'spacing'),
+       Return a VBox with label 'label' and the Gtk spin button on one line (spaced by 'spacing'),
        and the Gtk scale on an other."""
-    vbox = Gtk.VBox()
-    hbox = Gtk.HBox(spacing = spacing)
-    vbox.pack_start(hbox, False, False, 0)
-    hbox.pack_start(Gtk.Label(label = label, halign = Gtk.Align.START), True, True, 0)
-    hbox.pack_start(self.button, False, False, 0)
-    vbox.pack_start(self.scale, self.expand, self.expand, 0)
+    vbox = VBox(spacing = 0)
+    hbox = HBox(spacing = spacing)
+    vbox.pack(hbox)
+    hbox.pack(Gtk.Label(label = label, halign = Gtk.Align.START), expand = True, fill = True)
+    hbox.pack(self.button)
+    vbox.pack(self.scale, expand = self.expand, fill = self.expand)
     return vbox
 
-#
+###############
+# Text input. #
+###############
 
 class Entry(Signals, Gtk.Entry):
   """A custom Gtk entry with extended signal management."""
@@ -251,19 +286,13 @@ class Entry(Signals, Gtk.Entry):
     self.unblock_all_signals()
 
   def hbox(self, pre = None, post = None, spacing = 8):
-    """Return a Gtk HBox with Gtk widget 'pre', the entry, and Gtk widget 'post', spaced by 'spacing'.
+    """Return a HBox with Gtk widget 'pre', the entry, and Gtk widget 'post', spaced by 'spacing'.
        If strings, 'pre' and 'post' are converted into Gtk labels."""
-    hbox = Gtk.HBox(spacing = spacing)
-    if pre is not None:
-      if isinstance(pre, str): pre = Gtk.Label(pre)
-      hbox.pack_start(pre, False, False, 0)
-    hbox.pack_start(self, False, False, 0)
-    if post is not None:
-      if isinstance(post, str): post = Gtk.Label(post)
-      hbox.pack_start(post, False, False, 0)
-    return hbox
+    return make_hbox(self, pre, post, spacing, False)
 
-#
+##############
+# Notebooks. #
+##############
 
 class Notebook(Signals, Gtk.Notebook):
   """A custom Gtk notebook with extended signal management."""
