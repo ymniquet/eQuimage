21b02d12e8b584ab718ac51baf25e6dfd23a7461
ymniquet <ymniquet@gmail.com>
2024-09-01 10:00:02 +0200

    Version 1.6.0.

--- src/eQuimage/gui/settings.py
+++ src/eQuimage/gui/settings.py
@@ -2,7 +2,7 @@
 # This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 # Author: Yann-Michel Niquet (contact@ymniquet.fr).
-# Version: 1.5.2 / 2024.06.23
+# Version: 1.6.0 / 2024.09.01
 # GUI updated.
 
 """Settings window."""
