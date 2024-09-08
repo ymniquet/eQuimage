261b60d4e5fa29a187eccea01db939c6fa9b384c
ymniquet <ymniquet@gmail.com>
2024-09-08 19:52:49 +0200

    Added mtf and ghs stretches to pixelmath.

--- src/eQuimage/imageprocessing/pixelmath.py
+++ src/eQuimage/imageprocessing/pixelmath.py
@@ -10,6 +10,7 @@ import re
 from .defs import IMGTYPE
 from . import utils
 from . import colors
+from . import stretchfunctions as stretch
 
 class PixelMath:
   """Pixel math class."""
@@ -27,25 +28,21 @@ class PixelMath:
 
     import numpy as np
 
-    def midtone_stretch(image, midtone):
-      """Apply midtone stretch function with midtone 'midtone' to image 'image'."""
-      return (midtone-1.)*image/((2.*midtone-1.)*image-midtone) if midtone != .5 else image
-
     def value(image, midtone = .5):
       """Return the HSV value of image 'image' with midtone correction 'midtone'."""
-      return midtone_stretch(colors.hsv_value(image), midtone)
+      return mtf(colors.hsv_value(image), midtone)
 
     def luma(image, midtone = .5):
       """Return the luma of image 'image' with midtone correction 'midtone'."""
-      return midtone_stretch(colors.luma(image), midtone)
+      return mtf(colors.luma(image), midtone)
 
     def luminance(image, midtone = .5):
       """Return the luminance of image 'image' with midtone correction 'midtone'."""
-      return midtone_stretch(colors.lrgb_to_srgb(colors.srgb_luminance(image)), midtone)
+      return mtf(colors.lrgb_to_srgb(colors.srgb_luminance(image)), midtone)
 
     def lightness(image, midtone = .5):
       """Return the CIE lightness of image 'image' with midtone correction 'midtone'."""
-      return midtone_stretch(colors.srgb_lightness(image), midtone)
+      return mtf(colors.srgb_lightness(image), midtone)
 
     def scale(image, source, target):
       """Scale image 'image' by the ratio 'target'/'source'."""
@@ -55,9 +52,17 @@ class PixelMath:
       """Blend images 'image1' and 'image2' as (1-mix)*image1+mix*image2."""
       return (1.-mix)*image1+mix*image2
 
+    def mtf(image, midtone):
+      """Apply midtone stretch function with midtone 'midtone' to image 'image'."""
+      return (midtone-1.)*image/((2.*midtone-1.)*image-midtone) if midtone != .5 else image
+
+    def ghs(image, lnD1, B, SYP, SPP = 0., HPP = 1.):
+      """Apply generalized hyperbolic stretch function with parameters (lnD1, B, SYP, SPP,HPP) to image 'image'."""
+      return stretch.ghyperbolic_stretch_function(image, (lnD1, B, SYP, SPP, HPP, False))
+
     # Register the environment as globals.
 
-    globs.update({"np": np, "value": value, "luma": luma, "luminance": luminance, "lightness": lightness, "scale": scale, "blend": blend})
+    globs.update({"np": np, "value": value, "luma": luma, "luminance": luminance, "lightness": lightness, "scale": scale, "blend": blend, "mtf": mtf, "ghs": ghs})
 
     # Register all images as locals.
 
